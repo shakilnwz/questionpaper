d4fb50d7ed0ffbd3e46ae325b509f7babd5a4f9c
Shakil Nawaz <shakilnwz@gmail.com>
2025-10-07 20:32:47 +0600

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno(), vue(), tailwindcss()],
+    plugins: [deno(), vue(), tailwindcss()],
+    base: '/questionpaper/',
 })
